68cd559415749f524c443aaa5ed5c5eced90b3bd
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-06-01 18:39:39 +0000

    DataTypes Done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as Newer version 
+
+// alert (3 + 3) //we are using Node Js Not Browser
+console.log(3 + 3)
+console.log("som")
+
+let name = "soumitra Jena"  // String Types Data
+let age = 23   // Integer Types Data
+let LoggedIn = true //Boolean Types Data 
+
+// number => 2 to power 53
+// bigInt for the Bigger Numbers 
+// String = "" String Types 
+//Boolean = True/False 
+// null = standalone value 
+// Undifined => create the variable But Not assign
+// Symbol  => Unique
+
+
+
+// object
+
+
+console.log(typeof null);   // Object
+console.log(typeof undefined);  // Undefined Type
