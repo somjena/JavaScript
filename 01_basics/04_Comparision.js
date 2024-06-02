613b8f28e54d0ad55e226f7c883eb5183dcdb6a7
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-06-02 15:12:14 +0000

    Complete Comparision

---
+++ 01_basics/04_Comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//console.log("02">1);
+//console.log("2">1);
+
+//console.log(null > 0);
+//console.log(null == 0);
+//console.log(null >=0 );
+
+
+console.log(undefined == 0);   //false
+console.log(undefined > 0);   //false
+console.log(undefined < 0);  //false
+
+//Strict Check ===
+console.log("2"===2);
