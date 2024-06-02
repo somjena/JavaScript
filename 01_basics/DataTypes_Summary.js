ea0d7306f5300a4473055009490dd222a1e9876f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-06-02 15:36:08 +0000

    DataType Summary

---
+++ 01_basics/DataTypes_Summary.js
@@ -0,0 +1,39 @@
+// Primitive Type Of Data 
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const socre = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let UserEmail;
+
+
+const Id = Symbol('123')
+const AnotherId = Symbol('123')
+
+console.log(Id);
+console.log(AnotherId);
+
+console.log(Id === AnotherId);  //False
+
+const bigNumber = 6371326214n
+
+console.log(typeof bigNumber);
+
+// Reference Type [Non Premitive]
+
+ // Types: Array, Onjects, Functions
+
+ const heros = ["SahktiMan","Nagraj","doga"];
+ let myObj =
+ {
+    name:"som jena",
+    age :22,
+ }
+
+
+ const MyFunction = function(){
+    console.log("Hello World");
+ }
