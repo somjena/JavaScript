b2f436327b1567f9864570ed1cc0a6e67f82712f
Soumitra jena <131749408+somjena@users.noreply.github.com>
2024-06-02 06:48:17 +0000

    Completed The Conversion Of The Data Types

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,35 @@
+let score = "33"
+
+
+//console.log(typeof score);
+
+let valueInNumber = Number (score)
+
+
+//console.log(typeof valueInNumber);
+
+//"33" => 33
+//"33abc" => NaN Not A Number
+// true => 1 & False => 0
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+
+
+/* In the case of conversion of a Boolean Type of data ->
+if The Value is o {false}    &     if The Value is 1 {True}
+
+" "  Empty string => False
+"Som" string => True
+
+*/
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
