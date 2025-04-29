3e19314c13e0c38f90e16a75f964f84a8faa5438
somjena <jenasoumitrapvt@gmail.com>
2025-04-30 00:29:35 +0530

    prod.js

---
+++ Variable&DataType/let,var,const/Prod.js
@@ -0,0 +1,17 @@
+const product={
+  title:"Ball pen",
+  rating:4.5,
+  offer:5,
+  price:270
+};
+console.log(product);
+
+//Update the Data 
+
+product.title="Sahrp Blue Ball pen";
+product.price=390;
+product.offer=9;
+product.rating=3
+//Update the Same Object Existing Data 
+console.log(product);
+
