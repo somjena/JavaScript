0ea9d29500ac834045209e9446b8acb356e8cd13
somjena <jenasoumitrapvt@gmail.com>
2025-04-30 00:29:43 +0530

    profile.js

---
+++ Variable&DataType/let,var,const/profile.js
@@ -0,0 +1,13 @@
+const profile={
+  userName:"ShradhaKhapra",
+  isVerified:true,
+  posts:195,
+  followers:569000,
+  following:4,
+  profileName:"ShradhaKhapra",
+  threadId:"@ShradhaKhapra",
+  profession:"Entrepreneur",
+  about:"Apna College | Ex-Microsoft"
+
+}
+console.log(profile);
