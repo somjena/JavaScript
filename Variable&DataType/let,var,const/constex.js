b796cced6233c8a405c7809d953c3283cf2b46d1
somjena <jenasoumitrapvt@gmail.com>
2025-04-30 00:29:04 +0530

    Variables

---
+++ Variable&DataType/let,var,const/constex.js
@@ -0,0 +1,16 @@
+const accNo=65664649;
+//accNo=24446664;
+console.log(accNo);
+// We can not Reassign or Update the Data inside the const variable
+
+//This Will Throw this Error->
+
+/*TypeError: Assignment to constant variable.
+at Object.<anonymous> (E:\JavaScript\Variable&DataType\let,var,const\constex.js:2:6)
+at Module._compile (node:internal/modules/cjs/loader:1256:14)
+at Module._extensions..js (node:internal/modules/cjs/loader:1310:10)
+at Module.load (node:internal/modules/cjs/loader:1119:32)
+at Module._load (node:internal/modules/cjs/loader:960:12)
+at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:81:12)
+at node:internal/main/run_main_module:23:47
+*/
