cd7288c0919f1120f2b53f46e213ef782a6abc16
somjena <jenasoumitrapvt@gmail.com>
2025-04-29 19:25:23 +0530

    Let exmaple

---
+++ Variable&DataType/let,var,const/ex1.js
@@ -0,0 +1,11 @@
+let name ="som";
+let age=20;
+let gender = 'M';
+console.log(name+ " "+ age+ " "+gender);
+//console.log( typeof gender);
+//So Let Valiable Can not Declared again and agin but we can modified it or Updating it
+name="kadir khan";
+age=42;
+gender='F';
+console.log(name+ " "+ age+ " "+gender);
+
