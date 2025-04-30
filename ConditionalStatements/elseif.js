9cb58c49881511767e7b76d6fdacba96c3897aee
somjena <jenasoumitrapvt@gmail.com>
2025-05-01 01:33:50 +0530

    Else if .js

---
+++ ConditionalStatements/elseif.js
@@ -0,0 +1,28 @@
+let age  =17;
+if(age<18){
+  console.log("junior");
+  
+}else if(age>60){
+  console.log("Senior");
+  
+}else{
+  console.log("Middle");
+  
+}
+
+//Ex - 2 
+let mode="dark";
+let color;
+if(mode==="dark"){
+
+  color="black"
+}else if(mode==="blue"){
+  color ="pink";
+
+}else if(mode==="pink"){
+  color="white";
+}else{
+  color="white"
+}
+
+console.log(color);
