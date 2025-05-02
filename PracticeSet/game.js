1e1c37e53a611ce63b0cc61dfd3067f5eb1274f1
somjena <jenasoumitrapvt@gmail.com>
2025-05-02 16:46:17 +0530

    game.js

---
+++ PracticeSet/game.js
@@ -0,0 +1,8 @@
+
+alert("U Have to Enter a Number between 1-10 if u geuss u will win else loose")
+let num=prompt("Enter the Number");;
+let given  = 5;
+while(num!=given){
+  num =prompt("Enter the wrong Number try agein");
+}
+console.log("Congressssss !");
