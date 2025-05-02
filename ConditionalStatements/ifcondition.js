9b7d79fbad5f1ffb568e36893eddf35928bce001
somjena <jenasoumitrapvt@gmail.com>
2025-05-02 16:44:01 +0530

    conditional statement

---
+++ ConditionalStatements/ifcondition.js
@@ -0,0 +1,21 @@
+// Example - 1
+let age = 10;
+if(age>=18){
+  console.log("Eligible for Voting");
+  
+}
+if(age<=18){
+ console.log("Not Eligible for Voting");
+  
+}
+
+//Example - 2
+let mode = "dark";
+let color;
+if(mode==="dark"){
+  color="Black"
+}
+if(mode==="light"){
+  color="White";
+}
+console.log(color);
