40f60f465cd72faa025753c48fdd406932b8c8b8
somjena <jenasoumitrapvt@gmail.com>
2025-05-04 11:59:05 +0530

    Script.js

---
+++ PracticeSet/script.js
@@ -0,0 +1,9 @@
+alert("Get User to input a number using promt and check the number is multiple of 5 or not")
+let number =prompt("Enter The Number");
+if(number%5==0){
+  console.log(number,  " Is Multiple of 5");
+  
+}else{
+  console.log(number," is not Multiple of 5" );
+  
+}
