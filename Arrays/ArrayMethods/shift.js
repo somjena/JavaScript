77875ebdfe38c3268e14bbd5bc8cd78a020f698d
somjena <jenasoumitrapvt@gmail.com>
2025-06-22 10:57:03 +0530

    Shift Method Impl

---
+++ Arrays/ArrayMethods/shift.js
@@ -0,0 +1,5 @@
+let Products = ["Vinegar","Wheat","CornPowder"]
+console.log(`Before Using of Shift Method ${Products}`);
+Products.shift();
+console.log(`After Using of Shift Method ${Products}`);
+
